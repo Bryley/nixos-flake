d11c3603d68ad1bad662bd7aa1e9a8345a946159
Bryley Hayter <bryleyhayter@gmail.com>
2025-02-23 13:56:45 +1000

    . Small update

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -30,4 +30,13 @@ return {
             })
         end,
     },
+    {
+        -- Treesitter for surrealdb SQL
+        "dariuscorvus/tree-sitter-surrealdb.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            -- setup step
+            require("tree-sitter-surrealdb").setup()
+        end,
+    },
 }
